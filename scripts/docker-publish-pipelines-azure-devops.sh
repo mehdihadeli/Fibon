f738409f579cad3fe2452256a0260c1960ddad5a
mehdihadeli <mehdi.hadeli@outlook.com>
2020-06-12 15:26:18 +0430

    .

---
+++ scripts/docker-publish-pipelines-azure-devops.sh
@@ -0,0 +1,24 @@
+DOCKER_ENV=''
+DOCKER_TAG=''
+
+case "$(Build.SourceBranchName)" in
+  "master")
+    DOCKER_ENV=production
+    DOCKER_TAG=latest
+    ;;
+  "develop")
+    DOCKER_ENV=development
+    DOCKER_TAG=dev
+    ;;    
+esac
+
+docker login -u $(DOCKER_USERNAME) -p $(DOCKER_PASSWORD)
+
+docker build -f ./src/Fibon.Api/Dockerfile.$(DOCKER_ENV) -t fibon-api:$(DOCKER_TAG) ./src/Fibon.Api --no-cache
+docker build -f ./src/Fibon.Service/Dockerfile.$(DOCKER_ENV) -t fibon-service:$(DOCKER_TAG) ./src/Fibon.Service --no-cache
+
+docker tag fibon-api:$(DOCKER_TAG) $(DOCKER_USERNAME)/fibon-api:$(DOCKER_TAG)
+docker tag fibon-service:$(DOCKER_TAG) $(DOCKER_USERNAME)/fibon-service:$(DOCKER_TAG)
+
+docker push $(DOCKER_USERNAME)/fibon-api:$(DOCKER_TAG)
+docker push $(DOCKER_USERNAME)/fibon-service:$(DOCKER_TAG)
