d08a02d7a4cdc436d9bfaf59c462bef1b1cf9848
mehdihadeli <mehdi.hadeli@outlook.com>
2020-06-17 23:28:57 +0430

    .

--- scripts/docker-publish-pipelines-azure-devops.sh
+++ scripts/docker-publish-pipelines-azure-devops.sh
@@ -15,8 +15,8 @@ esac
 
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 
-docker build -f ./src/Fibon.Api/Dockerfile.$DOCKER_ENV -t fibon-api:$DOCKER_TAG ./src/Fibon.Api --no-cache
-docker build -f ./src/Fibon.Service/Dockerfile.$DOCKER_ENV -t fibon-service:$DOCKER_TAG ./src/Fibon.Service --no-cache
+docker build  -t fibon-api:$DOCKER_TAG  -f ./src/Fibon.Api/Dockerfile.$DOCKER_ENV ./src/Fibon.Api --no-cache
+docker build  -t fibon-service:$DOCKER_TAG -f ./src/Fibon.Service/Dockerfile.$DOCKER_ENV ./src/Fibon.Service --no-cache
 
 docker tag fibon-api:$DOCKER_TAG $DOCKER_USERNAME/fibon-api:$DOCKER_TAG
 docker tag fibon-service:$DOCKER_TAG $DOCKER_USERNAME/fibon-service:$DOCKER_TAG
--- scripts/dotnet-publish.sh
+++ scripts/dotnet-publish.sh
@@ -1,2 +1,2 @@
-dotnet publish ./src/Fibon.Api -c Release -o ./bin/Docker
-dotnet publish ./src/Fibon.Service -c Release -o ./bin/Docker
+dotnet publish ./src/Fibon.Api -c Release -o ./src/Fibon.Api/bin/Docker
+dotnet publish ./src/Fibon.Service -c Release -o ./src/Fibon.Service/bin/Docker
