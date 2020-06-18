70b6f0d76bc6e2e3a40e4549aa2417ad33bc8534
mehdihadeli <mehdi.hadeli@outlook.com>
2020-06-18 17:46:10 +0430

    .

--- scripts/dotnet-publish.sh
+++ scripts/dotnet-publish.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+# problem in creating out dir in .net 2 and 3
+# https://docs.microsoft.com/en-us/dotnet/core/tools/dotnet-publish
 dotnet publish ./src/Fibon.Api  -c Release -o ./src/Fibon.Api/bin/Docker  
 dotnet publish ./src/Fibon.Service  -c Release -o ./src/Fibon.Service/bin/Docker  
