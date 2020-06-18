ac8423c2e07a09ee90f4e7a19ab780258b4cf7c0
mehdihadeli <mehdi.hadeli@outlook.com>
2020-06-18 13:26:52 +0430

    .

--- scripts/dotnet-publish.sh
+++ scripts/dotnet-publish.sh
@@ -1,3 +1,3 @@
-cd ./src/Fibon.Api
-dotnet publish  -c Release -o ./bin/Docker
+# cd ./src/Fibon.Api
+dotnet publish ./src/Fibon.Api -c Release -o ./src/Fibon.Api/bin/Docker
 dotnet publish ./src/Fibon.Service -c Release -o ./src/Fibon.Service/bin/Docker
