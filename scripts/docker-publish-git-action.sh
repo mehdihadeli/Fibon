29b1306a04a7b9fca016252f2dfbf8636ddad28e
mehdihadeli <mehdi.hadeli@outlook.com>
2020-06-18 13:16:36 +0430

    .

--- scripts/docker-publish-git-action.sh
+++ scripts/docker-publish-git-action.sh
@@ -16,10 +16,10 @@ esac
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 
 docker build -f ./src/Fibon.Api/Dockerfile.$DOCKER_ENV -t fibon-api:$DOCKER_TAG ./src/Fibon.Api --no-cache
-docker build -f ./src/Fibon.Service/Dockerfile.$DOCKER_ENV -t fibon-service:$DOCKER_TAG ./src/Fibon.Service --no-cache
+# docker build -f ./src/Fibon.Service/Dockerfile.$DOCKER_ENV -t fibon-service:$DOCKER_TAG ./src/Fibon.Service --no-cache
 
 docker tag fibon-api:$DOCKER_TAG $DOCKER_USERNAME/fibon-api:$DOCKER_TAG
-docker tag fibon-service:$DOCKER_TAG $DOCKER_USERNAME/fibon-service:$DOCKER_TAG
+# docker tag fibon-service:$DOCKER_TAG $DOCKER_USERNAME/fibon-service:$DOCKER_TAG
 
 docker push $DOCKER_USERNAME/fibon-api:$DOCKER_TAG
-docker push $DOCKER_USERNAME/fibon-service:$DOCKER_TAG
+# docker push $DOCKER_USERNAME/fibon-service:$DOCKER_TAG
